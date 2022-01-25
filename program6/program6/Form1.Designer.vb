bf3f03cb3dd1b2fcdd346d70bf40d52349e36599
rutvik966 <86285948+rutvik966@users.noreply.github.com>
2022-01-25 09:15:09 +0530

    Add files via upload

---
+++ program6/program6/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.l2 = New System.Windows.Forms.ListBox()
+        Me.l1 = New System.Windows.Forms.ListBox()
+        Me.mess = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'l2
+        '
+        Me.l2.FormattingEnabled = True
+        Me.l2.ItemHeight = 25
+        Me.l2.Items.AddRange(New Object() {"A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "J", "K", "M", "N", "O", "P", "Q", "R", "U", "T", "V", "S", "W", "X", "Y", "Z"})
+        Me.l2.Location = New System.Drawing.Point(13, 13)
+        Me.l2.Name = "l2"
+        Me.l2.Size = New System.Drawing.Size(53, 654)
+        Me.l2.TabIndex = 0
+        '
+        'l1
+        '
+        Me.l1.FormattingEnabled = True
+        Me.l1.ItemHeight = 25
+        Me.l1.Items.AddRange(New Object() {"Amit", "Aakash", "Akshay", "bjgjg", "cjkkjd", "Rutvik", "utsav", "yagnik"})
+        Me.l1.Location = New System.Drawing.Point(99, 13)
+        Me.l1.Name = "l1"
+        Me.l1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.l1.Size = New System.Drawing.Size(180, 654)
+        Me.l1.TabIndex = 1
+        '
+        'mess
+        '
+        Me.mess.AutoSize = True
+        Me.mess.Location = New System.Drawing.Point(13, 697)
+        Me.mess.Name = "mess"
+        Me.mess.Size = New System.Drawing.Size(0, 25)
+        Me.mess.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(291, 742)
+        Me.Controls.Add(Me.mess)
+        Me.Controls.Add(Me.l1)
+        Me.Controls.Add(Me.l2)
+        Me.Name = "Form1"
+        Me.Text = " Form List"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents l2 As ListBox
+    Friend WithEvents l1 As ListBox
+    Friend WithEvents mess As Label
+End Class
---
+++ program6/program6/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub l2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles l2.SelectedIndexChanged
+        l1.ClearSelected()
+        For i As Integer = 0 To l1.Items.Count - 1
+            If l1.Items(i).ToString().StartsWith(l2.SelectedItem) = True Then
+                l1.SetSelected(i, True)
+            End If
+        Next
+
+        mess.text = "Total No Of Selected Items " & l1.SelectedItems.Count.ToString
+
+    End Sub
+End Class
