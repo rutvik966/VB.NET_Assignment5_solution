9833789777b062c4ff88d67e5c666d5b5763fa9f
rutvik966 <86285948+rutvik966@users.noreply.github.com>
2022-01-28 21:30:50 +0530

    Update Form1.vb

--- program7/program7/Form1.vb
+++ program7/program7/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class Form1
-
+Public Class Form1
+    Private Sub convertinto_SelectedIndexChanged(sender As Object, e As EventArgs) Handles convertinto.SelectedIndexChanged
+        If convertinto.SelectedIndex = 0 Then
+            ans.Text = enterrs.Text / 65
+        ElseIf convertinto.SelectedIndex = 1 Then
+            ans.Text = enterrs.Text / 0.6
+        ElseIf convertinto.SelectedIndex = 2 Then
+            ans.Text = enterrs.Text / 79
+        End If
+    End Sub
 End Class
